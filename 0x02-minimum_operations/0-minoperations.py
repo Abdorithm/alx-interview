fd65ff005e55fe95a24bb01ce57e52387d8b8667
Abdelrahman Magdi <abdomagdi300@gmail.com>
2024-06-17 00:01:11 +0300

    min ops

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Min Operations """
+
+
+def minOperations(n):
+    """ the fewest number of operations needed to
+    result in exactly n H characters """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
