4c375673e43c1a79ae983344fb72c4002551c2e0
Abdelrahman Magdi <abdomagdi300@gmail.com>
2024-09-05 00:46:42 +0300

    fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -27,13 +27,14 @@ def isWinner(x, nums):
     maria_wins = 0
     ben_wins = 0
 
-    for n in nums:
-        prime_count = prefix_sum[n]
-        if prime_count % 2 == 1:
-            maria_wins += 1
-        else:
-            ben_wins += 1
-
+    for round in range(x):
+        if round < len(nums):
+            n = nums[round]
+            prime_count = prefix_sum[n]
+            if prime_count % 2 == 1:
+                maria_wins += 1
+            else:
+                ben_wins += 1
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
